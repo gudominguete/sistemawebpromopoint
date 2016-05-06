772d7a8f1ea0b5de8c6ae0b365dc8a85dc5a71f0
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2016-05-06 09:15:19 -0300

    teste

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
     	<a href="admin.php">Página do Administrador</a><br>
         <a href="user.php">Página do usuário</a><br>
         <a href="empresa/index.php">Página do empresario</a><br>
-        
+        teste
 	</body>
 </html>
